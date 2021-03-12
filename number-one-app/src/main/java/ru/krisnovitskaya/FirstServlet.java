badf52bc0337030fd665cca89f64d8d9538bdce0
krisnovitskaya <voitage@yandex.ru>
2021-03-13 00:04:18 +0300

    FirstServlet app + log

---
+++ number-one-app/src/main/java/ru/krisnovitskaya/FirstServlet.java
@@ -0,0 +1,42 @@
+package ru.krisnovitskaya;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+
+public class FirstServlet implements Servlet {
+
+    private ServletConfig config;
+
+    private static final Logger logger = LoggerFactory.getLogger(FirstServlet.class);
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        logger.info("Servlet init");
+        this.config = config;
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return this.config;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        logger.info("Catch request");
+        servletResponse.getWriter().println("<h1>Hello! I am First Servlet</h1>");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
