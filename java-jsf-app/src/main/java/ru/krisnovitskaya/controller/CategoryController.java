d0b587bd814abb92027a83f980899288b2748028
krisnovitskaya <voitage@yandex.ru>
2021-04-03 07:52:36 +0300

    add customer and category

---
+++ java-jsf-app/src/main/java/ru/krisnovitskaya/controller/CategoryController.java
@@ -0,0 +1,40 @@
+package ru.krisnovitskaya.controller;
+
+import ru.krisnovitskaya.persist.Category;
+import ru.krisnovitskaya.persist.CategoryRepository;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@SessionScoped
+@Named
+public class CategoryController implements Serializable {
+    @Inject
+    private CategoryRepository categoryRepository;
+
+    private Category category;
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+    public List<Category> findAll(){ return categoryRepository.findAll();}
+
+    public void deleteCategory(Category category) {categoryRepository.delete(category.getId());}
+
+    public String saveCategory(){
+        categoryRepository.save(category);
+        return "/category.xhtml?faces-redirect=true";
+    }
+
+    public String addCategory(){
+        this.category = new Category();
+        return "/category_form.xhtml?faces-redirect=true";
+    }
+}
---
+++ java-jsf-app/src/main/java/ru/krisnovitskaya/persist/CategoryRepository.java
@@ -0,0 +1,43 @@
+package ru.krisnovitskaya.persist;
+
+import javax.annotation.PostConstruct;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class CategoryRepository {
+    private final Map<Long, Category> categoryMap = new ConcurrentHashMap<>();
+
+    private final AtomicLong identity = new AtomicLong(0);
+
+    @PostConstruct
+    public void init() {
+        save(new Category(null, "Category title 1"));
+        save(new Category(null, "Category title 2"));
+        save(new Category(null, "Category title 3"));
+        save(new Category(null, "Category title 4"));
+    }
+
+
+    public void save(Category category) {
+        if (category.getId() == null) {
+            category.setId(identity.incrementAndGet());
+        }
+        categoryMap.put(category.getId(), category);
+    }
+
+    public void delete(Long id) {
+        categoryMap.remove(id);
+    }
+
+    public Category findById(Long id) {
+        return categoryMap.get(id);
+    }
+
+    public List<Category> findAll() {
+        return new ArrayList<>(categoryMap.values());
+    }
+}
