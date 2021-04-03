094f79f6ba1affc786cdd2f466c68f046bb8821c
krisnovitskaya <voitage@yandex.ru>
2021-04-03 19:18:05 +0300

    fix

--- java-jsf-app/src/main/java/ru/krisnovitskaya/controller/CategoryController.java
+++ java-jsf-app/src/main/java/ru/krisnovitskaya/controller/CategoryController.java
@@ -2,9 +2,8 @@ package ru.krisnovitskaya.controller;
 
 import ru.krisnovitskaya.persist.Category;
 import ru.krisnovitskaya.persist.CategoryRepository;
-import ru.krisnovitskaya.persist.Customer;
-
 import javax.enterprise.context.SessionScoped;
+import javax.faces.event.ComponentSystemEvent;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
@@ -18,23 +17,26 @@ public class CategoryController implements Serializable {
 
     private Category category;
 
-    public Category getCategory() {
-        return category;
+    private List<Category> categoryList;
+
+    public void preloadData(ComponentSystemEvent componentSystemEvent) {
+        this.categoryList = categoryRepository.findAll();
     }
 
-    public void setCategory(Category category) {
-        this.category = category;
+    public List<Category> findAll() {
+        return categoryList;
     }
-    public List<Category> findAll(){ return categoryRepository.findAll();}
 
-    public void deleteCategory(Category category) {categoryRepository.delete(category.getId());}
+    public void deleteCategory(Category category) {
+        categoryRepository.delete(category.getId());
+    }
 
-    public String saveCategory(){
+    public String saveCategory() {
         categoryRepository.save(category);
         return "/category.xhtml?faces-redirect=true";
     }
 
-    public String addCategory(){
+    public String addCategory() {
         this.category = new Category();
         return "/category_form.xhtml?faces-redirect=true";
     }
@@ -42,5 +44,13 @@ public class CategoryController implements Serializable {
     public String editCategory(Category category) {
         this.category = category;
         return "/category_form.xhtml?faces-redirect=true";
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Category getCategory() {
+        return category;
     }
 }
