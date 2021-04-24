c901e687ccf79e00dd6135a4316433ffa1f4ab8e
krisnovitskaya <voitage@yandex.ru>
2021-04-24 07:40:39 +0300

    d

--- java-jsf-app/src/main/java/ru/krisnovitskaya/controller/CartController.java
+++ java-jsf-app/src/main/java/ru/krisnovitskaya/controller/CartController.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class CartController implements Serializable {
 
     @EJB
-    public CartService cartService;
+    private CartService cartService;
 
     private List<ProductRepr> cartList;
 
